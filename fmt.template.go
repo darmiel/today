e7b3def1eaef270f57f29f2239c980a2fb94ebf8
Daniel <71837281+darmiel@users.noreply.github.com>
2023-05-08 08:58:53 +0200

    feat: added some template context

--- fmt.template.go
+++ fmt.template.go
@@ -4,22 +4,33 @@ import (
 	"fmt"
 	"github.com/apognu/gocal"
 	"github.com/fatih/color"
+	"github.com/ralf-life/engine/engine"
 	"html/template"
 	"math"
 	"strings"
+	"time"
 )
 
 type templateContext struct {
-	Event    *gocal.Event
-	Relative string
-	Progress string
+	Event       *gocal.Event
+	Relative    string
+	RelativeRAW string
+	Progress    string
+	Start       *engine.CtxTime
+	End         *engine.CtxTime
+	IsCurrent   bool
 }
 
 func createProgressBar(event *gocal.Event) string {
 	// calculate total time in minutes
 	totalMins := event.End.Sub(*event.Start).Minutes()
+
 	// calculate elapsed time in minutes
-	currentMins := now.Sub(*event.Start).Minutes()
+	timeToStart := now.Sub(*event.Start)
+	if timeToStart < 0 {
+		timeToStart = time.Duration(0)
+	}
+	currentMins := timeToStart.Minutes()
 
 	if currentMins >= totalMins {
 		return "[ FINISHED             ]"
@@ -28,38 +39,53 @@ func createProgressBar(event *gocal.Event) string {
 	// progress of the lecture from 0 to 1
 	progress := float32(currentMins) / float32(totalMins)
 
-	progressBarPrompt := formatDuration(now.Sub(*event.Start)) + " / " + formatDuration(event.End.Sub(*event.Start))
+	progressBarPrompt := formatDuration(timeToStart) + " / " + formatDuration(event.End.Sub(*event.Start))
 
 	progressBarTotalLength := int(math.Max(20, float64(len(progressBarPrompt))))
 	progressBarCurrentLength := int(progress * float32(progressBarTotalLength))
 	progressBarPromptBefore, progressBarPromptAfter := split(progressBarPrompt, progressBarCurrentLength)
 
 	progressBarColorActive := color.New(color.FgBlack, color.BgHiWhite)
-	return progressBarColorActive.Sprintf("[ %s%s",
+
+	progressBarContentPrefix := fmt.Sprintf("[ %s%s",
 		progressBarPromptBefore,
-		strings.Repeat(" ", progressBarCurrentLength-len(progressBarPromptBefore))) +
+		strings.Repeat(" ", progressBarCurrentLength-len(progressBarPromptBefore)))
+	if currentMins > 0 {
+		progressBarContentPrefix = progressBarColorActive.Sprint(progressBarContentPrefix)
+	}
+	return progressBarContentPrefix +
 		fmt.Sprintf("%s%s ]",
 			progressBarPromptAfter,
 			strings.Repeat(" ", progressBarTotalLength-progressBarCurrentLength-len(progressBarPromptAfter)))
 }
 
 func createTemplateContext(tpl string, ctx *FormatContext) *templateContext {
-	var relative string
+	var (
+		relative    string
+		relativeRaw string
+	)
 	if ctx.isCurrent {
-		relative = fmt.Sprintf("%s remaining", formatDuration(ctx.event.End.Sub(now)))
+		relativeRaw = formatDuration(ctx.event.End.Sub(now))
+		relative = fmt.Sprintf("%s remaining", relativeRaw)
 	} else if now.After(*ctx.event.End) {
-		relative = fmt.Sprintf("%s ago", formatDuration(now.Sub(*ctx.event.End)))
+		relativeRaw = formatDuration(now.Sub(*ctx.event.End))
+		relative = fmt.Sprintf("%s ago", relative)
 	} else {
-		relative = fmt.Sprintf("in %s", formatDuration(ctx.event.Start.Sub(now)))
+		relativeRaw = formatDuration(ctx.event.Start.Sub(now))
+		relative = fmt.Sprintf("in %s", relativeRaw)
 	}
 	var progress string
 	if strings.Contains(tpl, "Progress") {
 		progress = createProgressBar(ctx.event)
 	}
 	return &templateContext{
-		Event:    ctx.event,
-		Relative: relative,
-		Progress: progress,
+		Event:       ctx.event,
+		Relative:    relative,
+		RelativeRAW: relativeRaw,
+		Progress:    progress,
+		Start:       &engine.CtxTime{Time: ctx.event.Start},
+		End:         &engine.CtxTime{Time: ctx.event.End},
+		IsCurrent:   now.After(*ctx.event.Start) && now.Before(*ctx.event.End),
 	}
 }
 
--- go.mod
+++ go.mod
@@ -6,19 +6,59 @@ require (
 	github.com/apognu/gocal v0.9.0
 	github.com/darmiel/golang-ical v0.0.0-20221121153313-bd526fc4018d
 	github.com/fatih/color v1.15.0
-	github.com/ralf-life/engine v0.0.0-20230425131010-4cf95eafebcb
+	github.com/ralf-life/engine v1.0.1-0.20230427065957-53deb5ed70f0
 	github.com/urfave/cli/v2 v2.25.1
 	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
 	github.com/ChannelMeter/iso8601duration v0.0.0-20150204201828-8da3af7a2a61 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/antonmedv/expr v1.12.5 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
 	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v9 v9.0.0-rc.2 // indirect
+	github.com/go-task/slim-sprig v0.0.0-20230315185526-52ccab3ef572 // indirect
+	github.com/gofiber/fiber/v2 v2.44.0 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/google/pprof v0.0.0-20230426061923-93006964c1fc // indirect
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/imroc/req/v3 v3.33.2 // indirect
+	github.com/klauspost/compress v1.16.5 // indirect
+	github.com/lucas-clemente/quic-go v0.31.1 // indirect
+	github.com/marten-seemann/qpack v0.3.0 // indirect
+	github.com/marten-seemann/qtls-go1-16 v0.1.5 // indirect
+	github.com/marten-seemann/qtls-go1-17 v0.1.2 // indirect
+	github.com/marten-seemann/qtls-go1-18 v0.1.4 // indirect
+	github.com/marten-seemann/qtls-go1-19 v0.1.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/onsi/ginkgo/v2 v2.9.2 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/quic-go/qpack v0.4.0 // indirect
+	github.com/quic-go/qtls-go1-18 v0.2.0 // indirect
+	github.com/quic-go/qtls-go1-19 v0.3.2 // indirect
+	github.com/quic-go/qtls-go1-20 v0.2.2 // indirect
+	github.com/quic-go/quic-go v0.34.0 // indirect
+	github.com/rivo/uniseg v0.4.4 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.46.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/crypto v0.8.0 // indirect
+	golang.org/x/exp v0.0.0-20230425010034-47ecfdc1ba53 // indirect
+	golang.org/x/mod v0.10.0 // indirect
+	golang.org/x/net v0.9.0 // indirect
 	golang.org/x/sys v0.7.0 // indirect
+	golang.org/x/text v0.9.0 // indirect
+	golang.org/x/tools v0.8.0 // indirect
 )
